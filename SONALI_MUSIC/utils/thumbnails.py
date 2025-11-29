ffd09158b47cecc5b576319b2ba2b17a1ecfd98c
JaniPrivetRepo <fffdfhhbnjjh@gmail.com>
2025-11-29 21:29:47 +0530

    Update thumbnails.py

--- SONALI_MUSIC/utils/thumbnails.py
+++ SONALI_MUSIC/utils/thumbnails.py
@@ -85,8 +85,8 @@ async def get_thumb(videoid):
         bold_font = ImageFont.truetype("SONALI_MUSIC/assets/font.ttf", 33)
 
         # Top-right "text"
-        text_size = draw.textsize("JANI BOTS    ", font=font)
-        draw.text((1280 - text_size[0] - 10, 10), "JANI BOTS    ", fill="yellow", font=font)
+        text_size = draw.textsize("HEMA MUSIC  ", font=font)
+        draw.text((1280 - text_size[0] - 10, 10), "HEMA MUSIC  ", fill="yellow", font=font)
 
         # Channel name + Views 
         draw.text(
