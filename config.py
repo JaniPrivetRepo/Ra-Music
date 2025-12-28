f218c6914e8ac08c7bfc72ee8d9d4f2d594ac184
JaniPrivetRepo <fffdfhhbnjjh@gmail.com>
2025-12-28 14:30:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 API_URL = getenv("API_URL", 'https://console.nexgenbots.xyz') #youtube song url
-API_KEY = getenv("API_KEY", 'NxGBNexGenBotsc5b366') # youtube song api ke
+API_KEY = getenv("API_KEY", '30DxNexGenBotsdc9b8a') # youtube song api ke
 
 PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-YukkiMusic-08-30")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Vip_Jani_OP")
