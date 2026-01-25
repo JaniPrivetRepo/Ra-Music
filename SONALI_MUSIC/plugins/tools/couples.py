a03e437c5ebb980cb7498f496caa3c007925f8d5
JaniPrivetRepo <fffdfhhbnjjh@gmail.com>
2026-01-25 23:55:48 +0530

    Update couples.py

--- SONALI_MUSIC/plugins/tools/couples.py
+++ SONALI_MUSIC/plugins/tools/couples.py
@@ -15,7 +15,7 @@ POLICE = [
     [
         InlineKeyboardButton(
             text="• ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ •",
-            url=f"https://t.me/HemaMusicBot?startgroup=true",
+            url=f"https://t.me/SizzuMusicBot?startgroup=true",
         ),
     ],
 ]
